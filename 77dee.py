e1b8d4320d00b235ad8107f40ce65d55aed4b586
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-07-04 15:21:12 +0530

    Update and rename 77deep.py to 77dee.py

---
+++ 77dee.py
@@ -0,0 +1,4 @@
+raji=int(input())
+for j in range(raji,l1+1):
+   if(ashtin1%j==0):
+      print(j,end=" ")
--- 77deep.py
+++
@@ -1,4 +0,0 @@
-ashtin17=int(input())
-for j in range(ashtin17,l1+1):
-   if(ashtin1%j==0):
-      print(j,end=" ")
