bf91a291be031e76f4b6b1fc2e322cb3fdbf652b
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-07-05 14:33:27 +0530

    Update 98deep.py

--- 98deep.py
+++ 98deep.py
@@ -1,10 +1,10 @@
-ld=int(input())
+deeptha=int(input())
 l=list(map(int,input().split()))
-lg=l[0]
-if ld==len(l):
+guvi=l[0]
+if deeptha==len(l):
   for i in range(1,len(l)):
-    if l[i]>lg:
-      lg=l[i]
+    if l[i]>guvi:
+      guvi=l[i]
     else:
       print(i-1)
       break
